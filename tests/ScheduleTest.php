9170fbb2b08151b087c6bc08f29176fbd660a793
Cesar <cesargb@gmail.com>
2021-10-10 12:28:39 +0200

    add tests to schedule

---
+++ tests/ScheduleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cesargb\File\Rotate\Test;
+
+use Illuminate\Console\Scheduling\Schedule;
+
+class ScheduleTest extends TestCase
+{
+    public function testHasSchedule()
+    {
+        $this->assertTrue($this->scheduleRegistered());
+    }
+
+    private function scheduleRegistered(): bool
+    {
+        return !is_null($this->schedule());
+    }
+
+    private function schedule()
+    {
+        return collect(app(Schedule::class)
+            ->events())
+            ->first(fn ($s) => $s->command, ' rotate:logs');
+    }
+}
